d628792a86596c3428fc5a1447fb4705c9faaf70
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-06 14:23:46 +0200

    add audio

--- src/drumMachine.jsx
+++ src/drumMachine.jsx
@@ -3,51 +3,53 @@ import React from 'react';
 function DrumMachine() {
     return (
       <div id="drum-machine">
-          <div id="display" />
-          <button className='drum-pad' id='Heater-1'>
-            <hudio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className='clip' id="Q" />
-            Q
-          </button>
-          <button className='drum-pad' id='W'>
-            <hudio src="" className='clip' id="W">
-            </hudio>
-            W
-          </button>
-          <button className='drum-pad' id='E'>
-            <hudio src="" className='clip' id="E">
-            </hudio>
-            E
-          </button>
-          <button className='drum-pad' id='A'>
-            <hudio src="" className='clip' id="A">
-            </hudio>
-            A
-          </button>
-          <button className='drum-pad' id='S'>
-            <hudio src="" className='clip' id="S">
-            </hudio>
-            S
-          </button>
-          <button className='drum-pad' id='D'>
-            <hudio src="" className='clip' id="D">
-            </hudio>
-            D
-          </button>
-          <button className='drum-pad' id='Z'>
-            <hudio src="" className='clip' id="Z">
-            </hudio>
-            Z
-          </button>
-          <button className='drum-pad' id='X'>
-            <hudio src="" className='clip' id="X">
-            </hudio>
-            X
-          </button>
-          <button className='drum-pad' id='C'>
-            <hudio src="" className='clip' id="C">
-            </hudio>
-            C
-          </button>
+        <div id="display" />
+
+        <button className='drum-pad' id='Heater-1'>
+          {/* <audio className='clip' id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio> */}
+          <audio  src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" controls  />
+          Q
+        </button>
+        <button className='drum-pad' id='W'>
+          <hudio src="" className='clip' id="W">
+          </hudio>
+          W
+        </button>
+        <button className='drum-pad' id='E'>
+          <hudio src="" className='clip' id="E">
+          </hudio>
+          E
+        </button>
+        <button className='drum-pad' id='A'>
+          <hudio src="" className='clip' id="A">
+          </hudio>
+          A
+        </button>
+        <button className='drum-pad' id='S'>
+          <hudio src="" className='clip' id="S">
+          </hudio>
+          S
+        </button>
+        <button className='drum-pad' id='D'>
+          <hudio src="" className='clip' id="D">
+          </hudio>
+          D
+        </button>
+        <button className='drum-pad' id='Z'>
+          <hudio src="" className='clip' id="Z">
+          </hudio>
+          Z
+        </button>
+        <button className='drum-pad' id='X'>
+          <hudio src="" className='clip' id="X">
+          </hudio>
+          X
+        </button>
+        <button className='drum-pad' id='C'>
+          <hudio src="" className='clip' id="C">
+          </hudio>
+          C
+        </button>
       </div>
     );
   }
