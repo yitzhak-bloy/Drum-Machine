434b01aa380db6bf2026c5ae9782e2e9e1fb5567
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-09 10:19:09 +0200

    adding List of sounds in a new file

---
+++ src/ListOfSounds.js
@@ -0,0 +1,49 @@
+const ListOfSounds = [
+    {
+      id: 'Heater-1',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      key: 'Q'
+    },
+    {
+      id: 'Heater-2',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      key: 'W'
+    },
+    {
+      id: 'Heater-3',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      key: 'E'
+    },
+    {
+      id: 'Heater-4_1',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      key: 'A'
+    },
+    {
+      id: 'Heater-6',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      key: 'S'
+    },
+    {
+      id: 'Dsc_Oh',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      key: 'D'
+    },
+    {
+      id: 'RP4_KICK_1',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      key: 'Z'
+    },
+    {
+      id: 'Cev_H2',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      key: 'X'
+    },
+    {
+      id: 'Chord_2',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+      key: 'C'
+    }
+]
+
+export default ListOfSounds
