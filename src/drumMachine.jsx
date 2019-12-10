6a995bcf78e5a5daa1f09d79cb96a7df361c1bfe
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-10 10:16:42 +0200

    Adds the option to tap the keyboard to hear the sounds

--- src/drumMachine.jsx
+++ src/drumMachine.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import ListOfSounds from './ListOfSounds'
 
 class DrumMachine extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.playMusic = this.playMusic.bind(this);
-  // }
 
   playMusic = (key) => {
     const sound = document.getElementById(key);
@@ -14,31 +10,54 @@ class DrumMachine extends Component {
 
   componentDidMount() {
     document.addEventListener('keydown', function(e) {
-      // if (e.keyCode === 81) {
-      //   document.getElementById('Q').play();
-      // }
       switch (e.keyCode) {
         case 81:
           document.getElementById('Q').play();
+          break;
+        case 87:
+          document.getElementById('W').play();
+          break;
+        case 69:
+          document.getElementById('E').play();
+          break;
+        case 65:
+          document.getElementById('A').play();
+          break;
+        case 83:
+          document.getElementById('S').play();
+          break;
+        case 68:
+          document.getElementById('D').play();
+          break;
+        case 90:
+          document.getElementById('Z').play();
+          break;
+        case 88:
+          document.getElementById('X').play();
+          break;
+        case 67:
+          document.getElementById('C').play();
+          break;
+        default:
       }
     });
   }
 
-    
   render() {
     return (
       <div id="drum-machine">
         <div id="display" />
         {ListOfSounds.map((item) => {
           return (
-          <button className='drum-pad' id={item.id} onClick={() => this.playMusic(item.key)}>
-            <audio className='clip' id={item.key} src={item.src}/ >
-            {item.key}
-          </button>)
+            <button className='drum-pad' id={item.id} onClick={() => this.playMusic(item.key)}>
+              <audio className='clip' id={item.key} src={item.src}/ >
+              {item.key}
+            </button>
+          )
         })}
       </div>
     );
   }
 }
   
-  export default DrumMachine;
+export default DrumMachine;
