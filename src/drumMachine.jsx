54d0b06f1c1b52c605c27d902ad2805e891ca060
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-11 12:21:09 +0200

    Modifying DrumMachine Component from Class Component to Component Function, by using react hooks to handle state.

--- src/drumMachine.jsx
+++ src/drumMachine.jsx
@@ -1,102 +1,74 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import ListOfSounds from './ListOfSounds'
 
-class DrumMachine extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-      nameOfSound: ''
-    }
-  }
+function DrumMachine() {
 
+  const [nameOfSound, setNameOfSound] = useState('');
 
-  playMusic = (key, id) => {
-    const sound = document.getElementById(key);
-    sound.play();
-    this.setState({
-      nameOfSound: id
-    })
-  };
-
-  componentDidMount() {
+  useEffect(() =>  {
     document.addEventListener('keydown', (e) => {
       switch (e.keyCode) {
         case 81:
           document.getElementById('Q').play();
-          this.setState({
-            nameOfSound: 'Heater-1'
-          })
+          setNameOfSound('Heater-1')
           break;
         case 87:
           document.getElementById('W').play();
-          this.setState({
-            nameOfSound: 'Heater-2'
-          })
+          setNameOfSound('Heater-2')
           break;
         case 69:
           document.getElementById('E').play();
-          this.setState({
-            nameOfSound: 'Heater-3'
-          })
+          setNameOfSound('Heater-3')
           break;
         case 65:
           document.getElementById('A').play();
-          this.setState({
-            nameOfSound: 'Heater-4_1'
-          })
+          setNameOfSound('Heater-4_1')
           break;
         case 83:
           document.getElementById('S').play();
-          this.setState({
-            nameOfSound: 'Heater-6'
-          })
+          setNameOfSound('Heater-6')
           break;
         case 68:
           document.getElementById('D').play();
-          this.setState({
-            nameOfSound: 'Dsc_Oh'
-          })
+          setNameOfSound('Dsc_Oh')
           break;
         case 90:
           document.getElementById('Z').play();
-          this.setState({
-            nameOfSound: 'RP4_KICK_1'
-          })
+          setNameOfSound('RP4_KICK_1')
           break;
         case 88:
           document.getElementById('X').play();
-          this.setState({
-            nameOfSound: 'Cev_H2'
-          })
+          setNameOfSound('Cev_H2')
           break;
         case 67:
           document.getElementById('C').play();
-          this.setState({
-            nameOfSound: 'Chord_2'
-          })
+          setNameOfSound('Chord_2')
           break;
         default:
       }
     });
-  }
+  })
 
-  render() {
-    return (
-      <div id="drum-machine">
-        <div id="display" >
-          {this.state.nameOfSound}
-        </div>
-        {ListOfSounds.map((item) => {
-          return (
-            <button className='drum-pad' id={item.id} key={item.key} onClick={() => this.playMusic(item.key, item.id)}>
-              <audio className='clip' id={item.key} src={item.src}/ >
-              {item.key}
-            </button>
-          )
-        })}
+  return (
+    <div id="drum-machine">
+      <div id="display" >
+        {nameOfSound}
       </div>
-    );
-  }
+      {ListOfSounds.map((item) => {
+        return (
+          <button className='drum-pad' id={item.id} key={item.key} onClick={() => {
+              const sound = document.getElementById(item.key);
+              sound.play();
+              setNameOfSound(item.id)
+            }}
+          >
+            <audio className='clip' id={item.key} src={item.src}/ >
+            {item.key}
+          </button>
+        )
+      })}
+    </div>
+  );
 }
   
 export default DrumMachine;
