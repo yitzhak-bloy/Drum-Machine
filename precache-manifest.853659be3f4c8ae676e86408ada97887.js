8357aab5fea859ea0cf8521582ac9dde773d459a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-15 09:19:17 +0200

    Updates

---
+++ precache-manifest.853659be3f4c8ae676e86408ada97887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e933dbe700fd32ce00483a9195ebb287",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "30d7e4c57c73a1445354",
+    "url": "/Drum-Machine/static/css/main.9f74fa27.chunk.css"
+  },
+  {
+    "revision": "a0c8a6a28220a8d2f34d",
+    "url": "/Drum-Machine/static/js/2.052a3b59.chunk.js"
+  },
+  {
+    "revision": "30d7e4c57c73a1445354",
+    "url": "/Drum-Machine/static/js/main.eabe5424.chunk.js"
+  },
+  {
+    "revision": "98fd5100399f19569be6",
+    "url": "/Drum-Machine/static/js/runtime-main.0e718b7d.js"
+  },
+  {
+    "revision": "c3ada4dd1fa640844e9b4fcbd16a19e0",
+    "url": "/Drum-Machine/static/media/josh-sorenson-MjIMc6uhwrE-unsplash.c3ada4dd.jpg"
+  }
+]);
